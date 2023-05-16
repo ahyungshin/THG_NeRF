9351ea4133ff5369ee4692c5c1fc80f2eed41db7
Ah-Hyung Shin <82164346+ahyungshin@users.noreply.github.com>
2023-05-16 19:19:45 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-#SBATCH --job-name=ll
-#SBATCH --gres=gpu:1
-#SBATCH -o slurm.out
-#SBATCH -p batch_agi
-#SBATCH --time=14-0
-#SBATCH --cpus-per-gpu=4
-#SBATCH --mem-per-gpu=20G
-
-. /data/shinahyung/anaconda3/etc/profile.d/conda.sh
-conda activate adnerf
-
 # training
 python -u run.py --model_path "TrainedModels" \
                 --model_file "model_Reso32HR.pth" \
